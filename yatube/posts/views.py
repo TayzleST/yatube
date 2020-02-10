2a05c75dfdc2f631b2e1b321df46493f96bf9b57
Alexander <spresi@mail.ru>
2020-02-10 16:31:24 +0300

    user can create his own new post

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,34 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Post
+from .models import Post, Group
+from .forms import PostForm
 
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:11]
     return render(request, 'index.html', {'posts': latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
+
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+        return render(request, 'new_post.html', {'form': form})
+    form = PostForm(request.POST)
+    return render(request, 'new_post.html', {'form': form})
+
+
+def  view_post(request):
+    pass   
 
