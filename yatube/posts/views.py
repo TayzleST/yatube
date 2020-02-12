88a03643d6f861bcf37629e600c8a44436d07e21
Alexander <spresi@mail.ru>
2020-02-12 20:40:53 +0300

    add paginator to group.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,8 +17,11 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, "group.html", {"group": group, "posts": posts})
+    post_list = Post.objects.filter(group=group).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10) # показывать по 10 записей на странице.
+    page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number) # получить записи с нужным смещением
+    return render(request, "group.html", {"group": group, 'page': page, 'paginator': paginator})
 
 
 def new_post(request):
