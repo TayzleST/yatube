43e53a2321ca08c65d22aedfcea2e067d91ebf31
Alexander <spresi@mail.ru>
2020-02-18 15:04:01 +0300

    send email to user after successful signup

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic.edit import CreateView
+from django.core import mail
 
 from .forms import CreationForm
 
+    
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = '/auth/login/'
+    success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        send_email(email, first_name, last_name)
+        return super().form_valid(form)
+
+def send_email(email, first_name, last_name):
+    if first_name == '' and last_name == '':
+        text = 'Благодарим'
+    else: 
+        text = ', благодарим'
+    mail.send_mail(
+            'Регистрация пользователя', 
+            f'{last_name} {first_name}{text} за регистрацию на нашем сайте!',
+            'yatube@mail.ru', 
+            [email],
+            fail_silently=False, 
+        )        
