cb3546f2a3e5885dab6439885878c15a0c1d94fc
Alexander <spresi@mail.ru>
2020-02-20 00:17:16 +0300

    added test for editing post

--- tests/tests.py
+++ tests/tests.py
@@ -2,9 +2,13 @@ from django.test import TestCase, Client, RequestFactory
 from django.core import mail
 import datetime as dt
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+
 
 from posts.models import Post, User
 from users.views import SignUp
+from posts.views import post_edit
+
 
 # Пользователь регистрируется и ему отправляется письмо с подтверждением регистрации
 class EmailTest(TestCase):
@@ -22,6 +26,17 @@ class EmailTest(TestCase):
                         'last_name first_name, благодарим за регистрацию на нашем сайте!')
         self.assertEqual(mail.outbox[0].from_email, 'yatube@mail.ru')
         self.assertEqual(mail.outbox[0].to, [email])
+        print(mail.outbox)
+
+    def test_send_email_2(self):
+        request = RequestFactory().post('/auth/signup/',
+            {'username': 'terminator', 'email': 'terminator@mail.ru', 'password': 'skynetMyLove'},
+            follow=True)
+        response = SignUp.as_view()(request)
+#        print(dir(response))
+        print(mail.outbox)
+
+    
 
 class PostViewTest(TestCase):
     '''
@@ -29,9 +44,8 @@ class PostViewTest(TestCase):
     '''
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(
-                         username="sarah", email="connor.s@skynet.com", password="12345"
-                        )
+        self.user = User.objects.create_user(username="sarah", 
+                                             email="connor.s@skynet.com", password="12345")
         self.post = Post.objects.create(text="It's driving me crazy!", author=self.user)
         
 
@@ -51,7 +65,7 @@ class PostViewTest(TestCase):
         self.assertEqual(response.status_code, 404)
         
 
-    #Авторизованный пользователь может опубликовать пост (new)
+    #Проверка, что авторизованный пользователь может опубликовать пост (new)
     def test_logged_in_user_can_create_post(self):
         # залогинем sarah
         self.client.login(username='sarah', password='12345')
@@ -59,7 +73,8 @@ class PostViewTest(TestCase):
         # ответ при переходе на страницу нового поста должен быть 200
         self.assertEqual(response.status_code, 200)
 
-    #Неавторизованный посетитель не может опубликовать пост (его редиректит на страницу входа)
+    #Проверка, что неавторизованный посетитель не может опубликовать пост 
+    # (его редиректит на страницу входа)
     def test_not_logged_in_cant_create_post(self):
         # sarah НЕ залогинена, должен быть код 302 и редирект в '/'
         response = self.client.get('/new/')
@@ -67,12 +82,44 @@ class PostViewTest(TestCase):
         self.assertRedirects(response, '/',)
 
 
-     # проверить авторизацию
-     # если авторизация успешна то ответ сраницы new/ 200
- #Неавторизованный посетитель не может опубликовать пост (его редиректит на страницу входа)
-     # если авторизация НЕ успешна то ответ сраницы new/ 302 (redirect)
- #После публикации поста новая запись появляется на главной странице сайта (index), на персональной странице пользователя (profile), и на отдельной странице поста (post)
-     # textContent на всех трех страницах
+    # Проверка, что после публикации поста новая запись появляется на 
+    # главной странице сайта (index), на персональной странице пользователя (profile),
+    # и на отдельной странице поста (post)
+    def test_post_displayed_on_all_pages(self):
+        # проверяем отображение поста на главной странице (index)
+        response = self.client.get('/')
+        self.assertEqual(str(response.context['paginator'].object_list[0]), "It's driving me crazy!")
+        # проверяем отображение поста на персональной странице пользователя (profile)
+        response = self.client.get('/sarah/')
+        self.assertEqual(str(response.context['paginator'].object_list[0]), "It's driving me crazy!")
+        # проверяем отображение поста на отдельной странице поста (post)
+        response = self.client.get(reverse('post', args=['sarah', 1]))
+        self.assertEqual(str(response.context['post']), "It's driving me crazy!")
+
+    
+    # Проверка, что авторизованный пользователь может отредактировать свой пост
+    # и его содержимое изменится на всех связанных страницах
+    def test_post_edit(self):
+        # залогинем sarah
+        self.client.login(username='sarah', password='12345')
+        # создаем запрос методом POST к редактируемой странице с измененным текстом 
+        request = RequestFactory().post('/sarah/1/edit/', {'text': "I'll be back!"}, follow=True)
+        request.user = self.user
+        post_edit(request, username='sarah', post_id=1)
+        # проверяем изменение поста на главной странице (index)
+        response = self.client.get('/')
+        self.assertEqual(str(response.context['paginator'].object_list[0]), "I'll be back!")
+        # проверяем изменение поста на персональной странице пользователя (profile)
+        response = self.client.get('/sarah/')
+        self.assertEqual(str(response.context['paginator'].object_list[0]), "I'll be back!")
+        # проверяем изменение поста на отдельной странице поста (post)
+        response = self.client.get(reverse('post', args=['sarah', 1]))
+        self.assertEqual(str(response.context['post']), "I'll be back!")
+        
+
+
+
+     
  #Авторизованный пользователь может отредактировать свой пост и его содержимое изменится на всех связанных страницах
      # для авторизованного пользователя доступна страница редактирована (нужно текущего пользователя выцепить)
      # для неаторизованного редиректит(ответ 302)
