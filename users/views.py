4da89da46541478e7116dd480de6a294f904a088
Alexander <spresi@mail.ru>
2020-02-19 13:31:09 +0300

    send_email collable was moved to SignUp class

--- users/views.py
+++ users/views.py
@@ -14,18 +14,18 @@ class SignUp(CreateView):
         email = form.cleaned_data['email']
         first_name = form.cleaned_data['first_name']
         last_name = form.cleaned_data['last_name']
-        send_email(email, first_name, last_name)
+        self.send_email(email, first_name, last_name)
         return super().form_valid(form)
 
-def send_email(email, first_name, last_name):
-    if first_name == '' and last_name == '':
-        text = 'Благодарим'
-    else: 
-        text = ', благодарим'
-    mail.send_mail(
-            'Регистрация пользователя', 
-            f'{last_name} {first_name}{text} за регистрацию на нашем сайте!',
-            'yatube@mail.ru', 
-            [email],
-            fail_silently=False, 
-        )        
+    def send_email(self, email, first_name=None, last_name=None):
+        if not first_name and not last_name:
+            text = 'Благодарим'
+        else: 
+            text = ', благодарим'
+        mail.send_mail(
+                'Регистрация пользователя', 
+                f'{last_name} {first_name}{text} за регистрацию на нашем сайте!',
+                'yatube@mail.ru', 
+                [email],
+                fail_silently=False, 
+            )        
